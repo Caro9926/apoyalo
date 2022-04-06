e7c792e7b042f6d64671c84f2e9148e3c13fc224
Caro9926 <carolinasaavedra01@gmail.com>
2022-04-06 00:04:30 -0500

    empleando props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Usuario from './componentes/Usuario';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Usuario firstName="Jane" lastName="Doe" age={45} hairColor="Black"/>
+      <Usuario firstName="John" lastName="Smith" age={88} hairColor="Brown"/>
+      <Usuario firstName="Millard" lastName="Filmore" age={50} hairColor="Brown"/>
+      <Usuario firstName="Maria" lastName="Smith" age={62} hairColor="Brown"/>
     </div>
   );
 }
---
+++ src/componentes/Usuario.js
@@ -0,0 +1,32 @@
+import React,{Component} from "react";
+import "./Usuario.css"
+class Usuario extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age:props.age
+        }
+    }
+
+    birthday = () => {
+        let actualAge = this.state.age;
+        actualAge++;
+        this.setState({age:actualAge});
+    }
+
+    render(){
+        const { firstName, lastName, hairColor } = this.props;
+        return (
+            <div className="card">
+                <h1>{lastName}, {firstName}</h1>
+                <div className="card-body">
+                    <p>Age: {this.state.age}</p>
+                    <p>Hair Color: {hairColor}</p>
+                </div>
+                <button onClick={this.birthday}>Birthday button for {firstName} {lastName}</button>
+            </div>
+        );
+    }
+}
+
+export default Usuario;
